a10ef59f895fc95c30ebced723032794594a20e7
lele94218 <terry.trxue@gmail.com>
2020-11-29 21:35:00 -0800

    initial gitignore and test shell

--- initial-xv6/test-getreadcount.sh
+++ initial-xv6/test-getreadcount.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if ! [[ -d ../../xv6-public ]]; then
+    echo "Your xv6 code shouldn't be found "
+    echo "along with ostep-projects directory."
+    exit 1
+fi
+
+(rm -rf ./src | true) && cp -r ../../xv6-public ./src
+
 if ! [[ -d src ]]; then
     echo "The src/ dir does not exist."
     echo "Your xv6 code should be in the src/ directory"
