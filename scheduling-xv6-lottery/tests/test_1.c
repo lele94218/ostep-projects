901494271a010c8649c507500d7cb6bf5f5c0bca
lele94218 <terry.trxue@gmail.com>
2020-12-20 19:23:21 -0800

    fix testing

--- scheduling-xv6-lottery/tests/test_1.c
+++ scheduling-xv6-lottery/tests/test_1.c
@@ -2,14 +2,16 @@
 #include "types.h"
 #include "user.h"
 
-int main(int argc, char* argv[]) {
+void fork_children(int id) {
+  if (id == 0)
+    return;
   int pid = fork();
   if (pid < 0) {
     printf(1, "fork failed...\n");
     exit();
   } else if (pid == 0) {
     // child goes here.
-    printf(1, "child_1 %d\n", getpid());
+    settickets(5);
     int i, j, a = 1, b = 2, tmp;
     for (i = 0; i < 10; ++i) {
       for (j = 0; j < 100; ++j) {
@@ -19,29 +21,16 @@ int main(int argc, char* argv[]) {
         printf(1, "%d %d\n", a, b);
       }
     }
-    printf(1, "child_1 ticks: %d\n", getticks());
+    printf(1, "XV6_TEST_OUTPUT:%d\n", id);
   } else {
-    // parent continues.
-    int next_pid = fork();
-    if (next_pid < 0) {
-      printf(1, "fork failed...\n");
-      exit();
-    } else if (next_pid == 0) {
-      printf(1, "child_2 %d\n", getpid());
-      int i, j, a = 1, b = 2, tmp;
-      for (i = 0; i < 10; ++i) {
-        for (j = 0; j < 100; ++j) {
-          tmp = a;
-          a = b;
-          b = tmp;
-          printf(1, "%d %d\n", a, b);
-        }
-      }
-      printf(1, "child_2 ticks: %d\n", getticks());
-    } else {
-      wait();
-    }
+    // Parent goes here.
+    fork_children((int)(id / 10));
     wait();
   }
+}
+
+int main(int argc, char* argv[]) {
+  fork_children(100);
+  wait();
   exit();
 }
